25168f51e28f54193e58c9c79fb4eeba01e88f14
ParvinaFS3 <parvinaisma@gmail.com>
2023-11-30 15:07:04 +0400

    first commit

---
+++ task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir söz daxil edin: ");
+        string input = Console.ReadLine();
+
+        PrintText(input);
+
+        Console.ReadLine(); 
+    }
+
+    static void PrintText(string text)
+    {
+        foreach (char character in text)
+        {
+            Thread thread = new Thread(() => Console.Write(character));
+            thread.Start();
+            Thread.Sleep(200);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
